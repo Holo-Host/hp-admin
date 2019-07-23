2ac07cf5ec75cef21eb53851910efb615e2d1b98
Robbie Carlton <robbie.carlton@gmail.com>
2019-07-23 17:30:49 -0600

    re introduce magic log statment

--- src/graphql-server/index.js
+++ src/graphql-server/index.js
@@ -2,6 +2,8 @@ import { makeExecutableSchema } from 'graphql-tools'
 import typeDefs from './schema.graphql'
 import resolvers from './resolvers'
 
+console.log('Removing this log statement breaks the app. Investigate why...')
+
 export const schema = makeExecutableSchema({
   typeDefs,
   resolvers
