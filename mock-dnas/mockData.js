5ac8e79072ddfeeacb07da4789280c9f495f3bd1
Robbie Carlton <robbie.carlton@gmail.com>
2019-07-25 17:34:28 -0600

    wip express server

---
+++ mock-dnas/mockData.js
@@ -0,0 +1,23 @@
+// data is a tree organized by instanceId > zome > function
+
+const data = {
+  hylo: {
+    people: {
+      get_me: {
+        id: 'fdkljsklj',
+        name: 'H.P. Owner',
+        avatarUrl: 'myface.png'
+      }
+    }
+  },
+  'happ-store': {
+    whoami: {
+      get_user: {
+        hash: 'browns',
+        name: 'H.P. Owner'
+      }
+    }
+  }
+}
+
+export default data
