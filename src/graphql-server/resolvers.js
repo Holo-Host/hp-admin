897f88177c04f1dcc5ec794730680aecf8bffeb4
Robbie Carlton <robbie.carlton@gmail.com>
2019-07-29 15:27:09 -0600

    clean up

--- src/graphql-server/resolvers.js
+++ src/graphql-server/resolvers.js
@@ -19,22 +19,12 @@ export const resolvers = {
 
     allHapps: async () => {
       const result = await HappStoreDnaInterface.happs.all()
-      // return result.map(happ => ({
-      //   id: happ.address,
-      //   ...pick(['title', 'thumbnailUrl', 'homepageUrl'], happ.appEntry),
-      //   // this is a kludge. need to clarify how we handle multiple dnas
-      //   hash: happ.appEntry.dnas[0].hash
-      // }))
-      return result.map(happ => {
-        const mappedHapp = {
-          id: happ.address,
-          ...pick(['title', 'thumbnailUrl', 'homepageUrl'], happ.appEntry),
-          // this is a kludge. need to clarify how we handle multiple dnas
-          hash: happ.appEntry.dnas[0].hash
-        }
-        console.log('TCL: mappedHapp', mappedHapp)
-        return mappedHapp
-      })
+      return result.map(happ => ({
+        id: happ.address,
+        ...pick(['title', 'thumbnailUrl', 'homepageUrl'], happ.appEntry),
+        // this is a kludge. need to clarify how we handle multiple dnas
+        hash: happ.appEntry.dnas[0].hash
+      }))
     }
   }
 }
