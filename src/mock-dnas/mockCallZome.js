da30772d54c26ab14d5e15ebe9d3f06b0717b3ef
Robbie Carlton <robbie.carlton@gmail.com>
2019-07-30 08:44:41 -0600

    clean up

--- src/mock-dnas/mockCallZome.js
+++ src/mock-dnas/mockCallZome.js
@@ -4,12 +4,7 @@ import { isFunction } from 'lodash/fp'
 export default function mockCallZome (instanceId, zome, zomeFunc) {
   return function (args) {
     const funcOrResult = mockData[instanceId][zome][zomeFunc]
-    let result
-    if (isFunction(funcOrResult)) {
-      result = funcOrResult(args)
-    } else {
-      result = funcOrResult
-    }
+    const result = isFunction(funcOrResult) ? funcOrResult(args) : funcOrResult
     return Promise.resolve(JSON.stringify({ Ok: result }))
   }
 }
