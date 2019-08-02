4fb503e93d5d9e7b9e1c95dc5019cd33caa21608
Lisa <ldjetton21@gmail.com>
2019-08-02 02:57:31 -0500

    patches to schema types

--- src/graphql-server/index.js
+++ src/graphql-server/index.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import { graphiqlExpress } from 'apollo-server-express';
+// import express from 'express'
+// import bodyParser from 'body-parser'
+// import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
 import { makeExecutableSchema } from 'graphql-tools'
 import typeDefs from './schema.graphql'
 import resolvers from './resolvers'
 
 console.log("If we remove this, your app might break!!", typeDefs)
 
-export const schema = makeExecutableSchema({
+const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 })
 
-// Initialize the app
-const app = express();
-
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: '/graphql',
-  }),
-);
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+// // Initialize the app
+// const app = express()
+//
+// // The GraphQL endpoint
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+//
+// // GraphiQL, a visual editor for queries
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+//
+// // Start the server
+// app.listen(3000, () => {
+//   console.log('Go to http://localhost:3000/graphiql to run queries!')
+// });
 
 export default schema
