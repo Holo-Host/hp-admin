28e571db62c5a85fb882e3ed5805d19a2e6e5b04
Robbie Carlton <robbie.carlton@gmail.com>
2019-08-05 17:01:07 -0600

    wip

--- src/graphql-server/index.js
+++ src/graphql-server/index.js
@@ -1,29 +1,12 @@
-// import express from 'express'
-// import bodyParser from 'body-parser'
-// import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
 import { makeExecutableSchema } from 'graphql-tools'
 import typeDefs from './schema.graphql'
 import resolvers from './resolvers'
 
-console.log('2 - If we remove this, your app might break!!', typeDefs)
+console.log('3 - If we remove this, your app might break!!', typeDefs)
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 })
-
-// // Initialize the app
-// const app = express()
-//
-// // The GraphQL endpoint
-// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
-//
-// // GraphiQL, a visual editor for queries
-// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-//
-// // Start the server
-// app.listen(3000, () => {
-//   console.log('Go to http://localhost:3000/graphiql to run queries!')
-// });
 
 export default schema
