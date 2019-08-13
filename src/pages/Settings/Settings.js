7bacf081c4eeb16a0ad03c5b6ec1429765bae506
Loren Johnson <lorenjohnson@gmail.com>
2019-08-13 12:32:51 -0700

    quick clean-up

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './Settings.module.css'
 
 export default function Settings () {
-  return <form style-name='settings-form'>
+  return <form styleName='settings-form'>
     <label data-for='ssh-access'>SSH Access</label>
     <input type='checkbox' name='ssh-access' />
 
     <label data-for='registration-email'>Registration Email</label>
-    <input type='checkbox' name='registration-email' />
+    <input name='registration-email' />
 
     <button name='factory-reset'>Factory Reset</button>
   </form>
