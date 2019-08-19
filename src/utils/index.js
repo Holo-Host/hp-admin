319b34f392949f510bb909a956547baebfd5dda3
Robbie Carlton <robbie.carlton@gmail.com>
2019-08-19 12:01:27 -0600

    bye bye bluebird

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,10 @@ export function bgImageStyle (url) {
   const escaped = url.replace(/([\(\)])/g, (match, $1) => '\\' + $1) // eslint-disable-line
   return { backgroundImage: `url(${escaped})` }
 }
+
+export async function promiseMap (array, fn) {
+  const resolvedArray = await array
+  const promiseArray = resolvedArray.map(fn)
+  const resolved = await Promise.all(promiseArray)
+  return resolved
+}
