48ac85dd13633641698660122d1e92374af49b09
Loren Johnson <lorenjohnson@gmail.com>
2019-09-03 13:20:16 -0700

    Initial pass

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Btn from 'components/Button'
+import './Dashboard.module.css'
 
 export default function Dashboard ({ me, happStoreUser, registerUser }) {
   return <div>
@@ -9,18 +10,31 @@ export default function Dashboard ({ me, happStoreUser, registerUser }) {
       <Link to='/menu' >Menu</Link>
     </div>
 
-    Here's the dashboard. It will have more information than this later.
+    <div styleName='linkBox'>
+      <h2><a href='/browse-happs'>Hosting</a></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
 
-    <p>
-      User data: {me && JSON.stringify(me)}
-    </p>
+    <div styleName='linkBox'>
+      <h2>Earnings</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
 
-    <p>
-      Happ Store User data: {happStoreUser && JSON.stringify(happStoreUser)}
-    </p>
+    <div styleName='linkBox'>
+      <h2>Settings</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
 
-    <p>
+    <div styleName='testing'>
       <Btn variant='primary' onClick={() => registerUser('Test User', 'testuserface.png')}>Register a test user</Btn>
-    </p>
+
+      <p>
+        <strong>User data:</strong> {me && JSON.stringify(me)}
+      </p>
+
+      <p>
+        <strong>Happ Store User data:</strong> {happStoreUser && JSON.stringify(happStoreUser)}
+      </p>
+    </div>
   </div>
 }
