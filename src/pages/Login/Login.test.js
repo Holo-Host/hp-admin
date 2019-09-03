3c9b0a33d6e4d66ef703e3bc68fa342270ebabdd
Robbie Carlton <robbie.carlton@gmail.com>
2019-09-03 12:42:34 -0600

    test Login page

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -1,135 +1,37 @@
 import React from 'react'
-import { render, fireEvent, within, act } from '@testing-library/react'
+import { render, fireEvent, act } from '@testing-library/react'
 import wait from 'waait'
-import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
-import { ApolloProvider } from '@apollo/react-hooks'
-import { MockedProvider } from '@apollo/react-testing'
-import apolloClient from 'apolloClient'
-import HappsQuery from 'graphql/HappsQuery.gql'
-import mockEnvoyInterface from 'data-interfaces/EnvoyInterface'
-import hhaInterface from 'data-interfaces/HhaDnaInterface'
-import { happs as hhaHapps } from 'mock-dnas/hha'
-import { appOne as appHoloFuel, appTwo as appHylo } from 'mock-dnas/happStore'
-import BrowseHapps from './BrowseHapps'
+import Login from './Login'
 
-jest.mock('data-interfaces/EnvoyInterface')
 // mocking Header because it depends on Router
 jest.mock('components/Header')
 
-function renderWithRouter (
-  ui,
-  {
-    route = '/',
-    history = createMemoryHistory({ initialEntries: [route] })
-  } = {}
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    history
-  }
-}
-
-describe('BrowseHapps Connected', () => {
+describe('Login', () => {
   it('renders', async () => {
-    let getAllByRole
-    await act(async () => {
-      ({ getAllByRole } = renderWithRouter(<ApolloProvider client={apolloClient}>
-        <BrowseHapps history={{}} />
-      </ApolloProvider>))
-      await wait(15)
-    })
+    const { getByPlaceholderText, getByText } = render(<Login history={{}} />)
 
-    const listItems = getAllByRole('listitem')
-    expect(listItems).toHaveLength(2)
-
-    const apps = [appHoloFuel, appHylo]
-
-    listItems.forEach((item, index) => {
-      const { getByText } = within(item)
-      expect(getByText(apps[index].appEntry.title)).toBeInTheDocument()
-      expect(getByText(apps[index].appEntry.description)).toBeInTheDocument()
-      if (index === 0) {
-        expect(getByText('Un-Host')).toBeInTheDocument()
-      }
-      if (index === 1) {
-        expect(getByText('Host')).toBeInTheDocument()
-      }
-    })
+    expect(getByPlaceholderText('Email address')).toBeInTheDocument()
+    expect(getByPlaceholderText('Password')).toBeInTheDocument()
+    expect(getByText('Login')).toBeInTheDocument()
   })
 
-  describe('HostButton', () => {
-    it('enables and disables happs', async () => {
-      hhaInterface.happs.enable = jest.fn()
-      let getAllByRole, queryAllByText
-      await act(async () => {
-        ({ getAllByRole, queryAllByText } = renderWithRouter(<ApolloProvider client={apolloClient}>
-          <BrowseHapps history={{}} />
-        </ApolloProvider>))
-        await wait(15)
+  describe('on filling out form and pressing login button', () => {
+    it('redirects to dashboard', async () => {
+      const push = jest.fn()
+      const email = 'a@b.com'
+      const password = 'fkldsjf'
+
+      const { getByPlaceholderText, getByText } = render(<Login history={{ push }} />)
+
+      act(() => {
+        fireEvent.change(getByPlaceholderText('Email address'), { target: { value: email } })
+        fireEvent.change(getByPlaceholderText('Password'), { target: { value: password } })
+        fireEvent.click(getByText('Login'))
       })
-
-      const listItems = getAllByRole('listitem')
-      expect(queryAllByText('Un-Host')).toHaveLength(1)
-      expect(queryAllByText('Host')).toHaveLength(1)
-
-      const { getByText: getByTextFromListItem } = within(listItems[0])
-      fireEvent.click(getByTextFromListItem('Un-Host'))
 
       await act(() => wait(0))
 
-      expect(queryAllByText('Un-Host')).toHaveLength(0)
-      expect(queryAllByText('Host')).toHaveLength(2)
-
-      fireEvent.click(getByTextFromListItem('Host'))
-
-      await act(() => wait(0))
-
-      expect(queryAllByText('Un-Host')).toHaveLength(1)
-      expect(queryAllByText('Host')).toHaveLength(1)
-      expect(hhaInterface.happs.enable).toHaveBeenCalledWith(hhaHapps[0].id)
-      expect(mockEnvoyInterface.happs.install).toHaveBeenCalledWith(hhaHapps[0].id)
-    })
-  })
-
-  const mocks = [
-    {
-      request: {
-        query: HappsQuery
-      },
-      result: {
-        data: {
-          happs: []
-        }
-      }
-    }
-  ]
-
-  describe('pricing button', () => {
-    it("calls history.push with '/pricing'", async () => {
-      const mockHistory = {
-        push: jest.fn()
-      }
-      const { getByText } = renderWithRouter(<MockedProvider mocks={mocks} addTypename={false}>
-        <BrowseHapps history={mockHistory} />
-      </MockedProvider>)
-      fireEvent.click(getByText('Manage Pricing'))
-      expect(mockHistory.push).toHaveBeenCalledWith('/pricing')
-    })
-  })
-
-  describe('hApp entry', () => {
-    it("navigates to '/browse-happs/APP_HASH' on click", async () => {
-      let getByText, history
-      await act(async () => {
-        ({ getByText, history } = renderWithRouter(<ApolloProvider client={apolloClient}>
-          <BrowseHapps history={{}} />
-        </ApolloProvider>))
-        await wait(0)
-      })
-
-      fireEvent.click(getByText('HoloFuel'))
-      expect(history.location.pathname).toBe('/browse-happs/QmHHAHappEntryAddressHash1')
+      expect(push).toHaveBeenCalledWith('/')
     })
   })
 })
