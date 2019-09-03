a5c326ebb90195a7ad16b5c18d4b8ff04be324e0
Loren Johnson <lorenjohnson@gmail.com>
2019-09-03 15:19:15 -0700

    3984 - Dashboard with links to features (#34)

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,26 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Btn from 'components/Button'
+import Header from 'components/Header'
+import { Link } from 'react-router-dom'
+import './Dashboard.module.css'
 
 export default function Dashboard ({ me, happStoreUser, registerUser }) {
-  return <div>
+  return <div styleName='container'>
+    <Header title='My HoloPort' />
 
-    <div>
-      <Link to='/menu' >Menu</Link>
+    <div styleName='linkBox'>
+      <h2><Link to='/browse-happs'>Hosting</Link></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
 
-    Here's the dashboard. It will have more information than this later.
+    <div styleName='linkBox'>
+      <h2><Link to='/earnings'>Earnings</Link></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
 
-    <p>
-      User data: {me && JSON.stringify(me)}
-    </p>
+    <div styleName='linkBox'>
+      <h2><Link to='/settings'>Settings</Link></h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
 
-    <p>
-      Happ Store User data: {happStoreUser && JSON.stringify(happStoreUser)}
-    </p>
-
-    <p>
+    <div styleName='testing'>
       <Btn variant='primary' onClick={() => registerUser('Test User', 'testuserface.png')}>Register a test user</Btn>
-    </p>
+
+      <p>
+        <strong>User data:</strong> {me && JSON.stringify(me)}
+      </p>
+
+      <p>
+        <strong>Happ Store User data:</strong> {happStoreUser && JSON.stringify(happStoreUser)}
+      </p>
+    </div>
   </div>
 }
