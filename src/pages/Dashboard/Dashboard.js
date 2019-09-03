a086f9918dac9f8b70f4f7f993644b7472d3a0fa
Loren Johnson <lorenjohnson@gmail.com>
2019-09-03 15:18:14 -0700

    Use router

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Btn from 'components/Button'
 import Header from 'components/Header'
+import { Link } from 'react-router-dom'
 import './Dashboard.module.css'
 
 export default function Dashboard ({ me, happStoreUser, registerUser }) {
@@ -8,17 +9,17 @@ export default function Dashboard ({ me, happStoreUser, registerUser }) {
     <Header title='My HoloPort' />
 
     <div styleName='linkBox'>
-      <h2><a href='/browse-happs'>Hosting</a></h2>
+      <h2><Link to='/browse-happs'>Hosting</Link></h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
 
     <div styleName='linkBox'>
-      <h2><a href='/earnings'>Earnings</a></h2>
+      <h2><Link to='/earnings'>Earnings</Link></h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
 
     <div styleName='linkBox'>
-      <h2><a href='/settings'>Settings</a></h2>
+      <h2><Link to='/settings'>Settings</Link></h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
 
