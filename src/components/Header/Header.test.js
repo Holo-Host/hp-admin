60fd1d0b1e5ececbf5d15fcee60dd0a8be8f1ebd
Daniel Szymanek <danekszy@users.noreply.github.com>
2019-09-04 16:58:11 +0200

    Add tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 // testing the named export Header rather than the default export which is wrapped in withRouter
 import { Header } from './Header'
 import { title as menuIconTitle } from 'components/icons/MenuIcon'
 import { title as backIconTitle } from 'components/icons/BackIcon'
+
+const renderHeader = (
+  props,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) => ({
+  ...render(
+    <Router history={history}>
+      <Header {...props} />
+    </Router>
+  ),
+  history
+})
 
 it('should render the title and a menu icon', () => {
   const props = {
     title: 'the title',
     history: { push: jest.fn() }
   }
-  const { getByText, getByTestId } = render(
-    <Header {...props} />)
+  const { getByText, getByTestId } = renderHeader(props)
 
   expect(getByText(props.title)).toBeInTheDocument()
   expect(getByText(menuIconTitle)).toBeInTheDocument()
@@ -28,8 +45,7 @@ describe('with backTo defined', () => {
       backTo: '/previous-page',
       history: { push: jest.fn() }
     }
-    const { getByText, getByTestId } = render(
-      <Header {...props} />)
+    const { getByText, getByTestId } = renderHeader(props)
 
     expect(getByText(props.title)).toBeInTheDocument()
     expect(getByText(backIconTitle)).toBeInTheDocument()
