e45752d8c259c76fe9ecf1bd086edf84985fb844
Lisa <ldjetton21@gmail.com>
2019-09-04 23:04:57 -0500

    removed comments in holofuel dashboard test

--- src/pages/HoloFuel/HoloFuelDashboard.test.js
+++ src/pages/HoloFuel/HoloFuelDashboard.test.js
@@ -4,7 +4,6 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import wait from 'waait'
 import apolloClient from 'apolloClient'
 import HoloFuelDashboard from './HoloFuelDashboard'
-// import { transactionList } from 'mock-dnas/holofuel'
 
 describe('HoloFuel Dashboard is connected', () => {
   it('renders', async () => {
@@ -23,8 +22,6 @@ describe('HoloFuel Dashboard is connected', () => {
     const listItems = getAllByRole('heading')
     expect(listItems).toHaveLength(7)
 
-    // expect(getByText(transactionList.ledger.balance)).toBeInTheDocument()
-    // expect(getByText(transactionList.ledger.payable)).toBeInTheDocument()
     expect(getByText('HoloFuel Dashboard')).toBeInTheDocument()
     expect(getByText('Initiate HoloFuel Transaction')).toBeInTheDocument()
   })
