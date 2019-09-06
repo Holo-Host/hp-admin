d4ee935de4e456714bda8444018f1ff8f4b64f5b
Robbie Carlton <robbie.carlton@gmail.com>
2019-09-06 11:26:15 -0600

    add simple HoloFuel root

---
+++ src/pages/HFDashboard/index.js
@@ -0,0 +1,4 @@
+import Component from './HFDashboard'
+import connector from './HFDashboard.connector'
+
+export default connector(Component)
---
+++ src/pages/HoloFuelPrimaryLayout/index.js
@@ -0,0 +1,3 @@
+import Component from './HoloFuelPrimaryLayout'
+
+export default Component
