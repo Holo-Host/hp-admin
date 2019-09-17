3967bda84cd9f106121f8fa439c4cf2d9b3bc182
Daniel Szymanek <danekszy@users.noreply.github.com>
2019-09-17 21:50:19 +0200

    Fix Tos test

--- src/pages/Tos/Tos.test.js
+++ src/pages/Tos/Tos.test.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { render, fireEvent, act } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 import wait from 'waait'
 import Tos from './Tos'
 
+const renderTos = (
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) => ({
+  ...render(
+    <Router history={history}>
+      {ui}
+    </Router>
+  ),
+  history
+})
+
 describe('Tos', () => {
   it('renders tos modal', () => {
-    const { getByText } = render(<Tos history={{}} />)
+    const { getByText } = renderTos(<Tos history={{}} />)
 
     expect(getByText('Terms of Service')).toBeInTheDocument()
   })
 
   it('goes back to settings page on close', async () => {
     const push = jest.fn()
-    const { getByText } = render(<Tos history={{ push }} />)
+    const { getByText } = renderTos(<Tos history={{ push }} />)
 
     fireEvent.click(getByText('Close'))
     await act(() => wait(0))
