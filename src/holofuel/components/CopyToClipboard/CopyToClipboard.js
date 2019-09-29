29cdb29076263db864d1abe7f8684096006a77e8
Lisa <ldjetton21@gmail.com>
2019-09-29 18:07:50 -0500

    removed tooltip from copyToClipboard component

--- src/holofuel/components/CopyToClipboard/CopyToClipboard.js
+++ src/holofuel/components/CopyToClipboard/CopyToClipboard.js
@@ -1,6 +1,5 @@
 import { string } from 'prop-types'
-import React, { useState } from 'react'
-import ReactTooltip from 'react-tooltip'
+import React from 'react'
 import copy from 'copy-to-clipboard'
 import './CopyToClipboard.module.css'
 import useFlashMessageContext from 'holofuel/contexts/useFlashMessageContext'
@@ -10,7 +9,6 @@ export default function CopyToClipboard ({
   hash,
   nickname,
   isMe,
-  toolTipId,
   children
 }) {
   if (!nickname)nickname = `${presentAgentId(hash)}'s`
@@ -18,27 +16,14 @@ export default function CopyToClipboard ({
   else nickname = `${nickname}'s`
   const { newMessage } = useFlashMessageContext()
 
-  const [copied, setCopied] = useState(false)
   const copyHash = () => {
     console.log('COPYING HASH : ', hash)
     copy(hash)
-    setCopied(true)
     newMessage(`${nickname} HoloFuel Agent ID has been copied!`, 5000)
   }
 
-  return <div styleName='copy-item'>
-    <div data-for={toolTipId} data-tip='' onClick={copyHash} data-testid='hash-display'>
-      {children}
-    </div>
-    <ReactTooltip
-      id={toolTipId}
-      delayShow={250}
-      afterHide={() => setCopied(false)}
-      getContent={() => hash
-        ? `${nickname} ID: ${hash} - ${copied ? 'Copied to clipboard' : 'Click to copy'}`
-        : null
-      }
-    />
+  return <div onClick={copyHash} data-testid='hash-display' styleName='copy-item'>
+    {children}
   </div>
 }
 
