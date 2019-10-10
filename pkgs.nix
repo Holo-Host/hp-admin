03891d5e628e84366b6db2de060864cd42cf14cb
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-10-10 14:01:23 +0000

    Repin Holo Nixpkgs to 393796a484f5843f71db832a17beeb819a06bc9f

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/31d521ccc5c2b54d057092c552c492d17ceabd50.tar.gz";
-  sha256 = "1mc2ci7xwb7jfg7bz87886mqfwhi12ad6ghp9326s0jy1l7l9ibb";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/393796a484f5843f71db832a17beeb819a06bc9f.tar.gz";
+  sha256 = "0z051kgzvwffb5wg326iy1vb3zd8b0m14pdhbffzp3a1bim40n4k";
 })
