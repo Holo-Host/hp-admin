e254cfe434dfb1ca35d476a3fdd355863e5276a1
Robbie Carlton <robbie.carlton@gmail.com>
2019-10-10 18:22:52 -0600

    use renderAndWait in inbox.test

---
+++ src/utils/test-utils.js
@@ -0,0 +1,11 @@
+import { render, act } from '@testing-library/react'
+import wait from 'waait'
+
+export async function renderAndWait (ui, ms = 0, options = {}) {
+  let queries
+  await act(async () => {
+    queries = render(ui, options)
+    await wait(ms)
+  })
+  return queries
+}
