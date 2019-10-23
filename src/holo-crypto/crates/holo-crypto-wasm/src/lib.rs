f94715bdebc3f1da9fedd16141c9ff62d6eb4151
Willem Olding <willemolding@gmail.com>
2019-10-23 15:19:07 +1100

    fmt

--- src/holo-crypto/crates/holo-crypto-wasm/src/lib.rs
+++ src/holo-crypto/crates/holo-crypto-wasm/src/lib.rs
@@ -2,10 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-use holo_crypto::{
-	error::HoloCryptoError,
-	KeyPair,
-};
+use holo_crypto::{error::HoloCryptoError, KeyPair};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -19,18 +16,17 @@ pub struct AdminKeyPair(KeyPair);
 #[wasm_bindgen]
 impl AdminKeyPair {
     #[wasm_bindgen(constructor)]
-	pub fn new(email: &str, pass: &str, hc_pub_key: &str) -> Result<AdminKeyPair, JsValue> {
-		let combined_passphrase = pass.to_owned() + email;
-		Ok(Self(KeyPair::new(&combined_passphrase, &hc_pub_key)?))
-	}
+    pub fn new(email: &str, pass: &str, hc_pub_key: &str) -> Result<AdminKeyPair, JsValue> {
+        let combined_passphrase = pass.to_owned() + email;
+        Ok(Self(KeyPair::new(&combined_passphrase, &hc_pub_key)?))
+    }
 
-	#[wasm_bindgen]
-	/// Returns a base64 encoded signature of the payload
-	pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
-		Ok(self.0.sign(payload)?)
-	}
+    #[wasm_bindgen]
+    /// Returns a base64 encoded signature of the payload
+    pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
+        Ok(self.0.sign(payload)?)
+    }
 }
-
 
 #[wasm_bindgen]
 pub struct WebUserKeyPair(KeyPair);
@@ -38,14 +34,19 @@ pub struct WebUserKeyPair(KeyPair);
 #[wasm_bindgen]
 impl WebUserKeyPair {
     #[wasm_bindgen(constructor)]
-	pub fn new(email: &str, pass: &str, salt: &str, happ_hash: &str) -> Result<WebUserKeyPair, JsValue> {
-		let combined_passphrase = pass.to_owned() + email + happ_hash;
-		Ok(Self(KeyPair::new(&combined_passphrase, salt)?))
-	}
+    pub fn new(
+        email: &str,
+        pass: &str,
+        salt: &str,
+        happ_hash: &str,
+    ) -> Result<WebUserKeyPair, JsValue> {
+        let combined_passphrase = pass.to_owned() + email + happ_hash;
+        Ok(Self(KeyPair::new(&combined_passphrase, salt)?))
+    }
 
-	#[wasm_bindgen]
-	/// Returns a base64 encoded signature of the payload
-	pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
-		Ok(self.0.sign(payload)?)
-	}
+    #[wasm_bindgen]
+    /// Returns a base64 encoded signature of the payload
+    pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
+        Ok(self.0.sign(payload)?)
+    }
 }
--- src/holo-crypto/crates/holo-crypto-wasm/tests/web.rs
+++ src/holo-crypto/crates/holo-crypto-wasm/tests/web.rs
@@ -3,50 +3,39 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
 use holo_crypto_wasm::{AdminKeyPair, WebUserKeyPair};
+use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn admin_can_derive_key_and_sign() {
-	let akp = AdminKeyPair::new(
-		"some@email.test",
-		"p4ssw0rd",
-		"HCA01adefasdtbeodbgahfawnf"
-	).expect("Could not generate keypair");
+    let akp = AdminKeyPair::new("some@email.test", "p4ssw0rd", "HCA01adefasdtbeodbgahfawnf")
+        .expect("Could not generate keypair");
 
-	assert_eq!(
-		akp.sign("message").expect("Could not sign payload").len(),
-		88, // 64 bytes base64 encoded = 88 bits
-	)
+    assert_eq!(
+        akp.sign("message").expect("Could not sign payload").len(),
+        88, // 64 bytes base64 encoded = 88 bits
+    )
 }
 
 #[wasm_bindgen_test]
 fn admin_will_error_if_salt_too_short() {
-	assert!(
-		AdminKeyPair::new("some@email.test", "pwd", "HCA01..").is_err()
-	)
+    assert!(AdminKeyPair::new("some@email.test", "pwd", "HCA01..").is_err())
 }
 
 #[wasm_bindgen_test]
 fn webuser_can_derive_key_and_sign() {
-	let akp = WebUserKeyPair::new(
-		"some@email.test",
-		"p4ssw0rd",
-		"saltysalt",
-		"QmDnaHash..."
-	).expect("Could not generate keypair");
+    let akp = WebUserKeyPair::new("some@email.test", "p4ssw0rd", "saltysalt", "QmDnaHash...")
+        .expect("Could not generate keypair");
 
-	assert_eq!(
-		akp.sign("message").expect("Could not sign payload").len(),
-		88, // 64 bytes base64 encoded = 88 bits
-	)
+    assert_eq!(
+        akp.sign("message").expect("Could not sign payload").len(),
+        88, // 64 bytes base64 encoded = 88 bits
+    )
 }
 
 #[wasm_bindgen_test]
 fn webuser_will_error_if_salt_too_short() {
-	assert!(
-		WebUserKeyPair::new("some@email.test", "pwd", "HCA01..", "QmDnaHash...").is_err()
-	)
+    assert!(WebUserKeyPair::new("some@email.test", "pwd", "HCA01..", "QmDnaHash...").is_err())
 }
