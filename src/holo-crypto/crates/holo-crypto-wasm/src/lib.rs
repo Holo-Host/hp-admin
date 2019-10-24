acb5c68f828fef6d6a06f8ec91ab38b55709fa4c
Willem Olding <willemolding@gmail.com>
2019-10-25 10:00:12 +1100

    start switch to holo-config-core

--- src/holo-crypto/crates/holo-crypto-wasm/src/lib.rs
+++ src/holo-crypto/crates/holo-crypto-wasm/src/lib.rs
@@ -1,8 +1,10 @@
 mod utils;
 
+use std::convert::TryInto;
 use wasm_bindgen::prelude::*;
 
-use holo_crypto::{KeyPair};
+// use holo_crypto::{KeyPair};
+use holo_config_core::Config;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,20 +13,27 @@ use holo_crypto::{KeyPair};
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub struct AdminKeyPair(KeyPair);
+pub struct AdminKeyPair(Config);
 
 #[wasm_bindgen]
 impl AdminKeyPair {
     #[wasm_bindgen(constructor)]
     pub fn new(email: &str, pass: &str, hc_pub_key: &str) -> Result<AdminKeyPair, JsValue> {
-        let combined_passphrase = pass.to_owned() + email;
-        Ok(Self(KeyPair::new(&combined_passphrase, &hc_pub_key)?))
+        // let combined_passphrase = pass.to_owned() + email;
+        // Ok(Self(KeyPair::new(&combined_passphrase, &hc_pub_key)?))
+        let seed: [u8; 32] = hc_pub_key.as_bytes().try_into().unwrap();
+        let (config, _pub_key) = Config::new(email.to_string(), pass.to_string(), Some(seed)).unwrap();
+        Ok(Self(config))
     }
 
     #[wasm_bindgen]
     /// Returns a base64 encoded signature of the payload
     pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
-        Ok(self.0.sign(payload)?)
+        match self.0 {
+            Config::V1{admin, ..} => {
+                Ok(admin.public_key.sign(payload)?)
+            }
+        }
     }
 }
 
