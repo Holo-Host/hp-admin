9ce26a92d538a1dca1672d9bf91bc37a52ea396d
Willem Olding <willemolding@gmail.com>
2019-10-28 16:53:14 +1100

    refactor checks, get tests passing

--- src/holo-crypto-wasm/src/lib.rs
+++ src/holo-crypto-wasm/src/lib.rs
@@ -1,16 +1,65 @@
 mod utils;
 
-
 use wasm_bindgen::prelude::*;
 
 // use holo_crypto::{KeyPair};
 use holo_config_core::{admin_keypair_from, keypair_from, Keypair, PublicKey};
+
+const ARGON2_MAX_PASSPHRASE_LEN: usize = (2 ^ 32) - 1;
+const ARGON2_MAX_SALT_LEN: usize = (2 ^ 32) - 1;
+const ARGON2_MIN_SALT_LEN: usize = 8;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[derive(Debug)]
+enum HoloCryptoError {
+    Custom(String),
+}
+
+impl From<HoloCryptoError> for JsValue {
+    fn from(e: HoloCryptoError) -> Self {
+        JsValue::from_str(&format!("{:?}", e).to_string())
+    }
+}
+
+fn check_passphrase(passphrase_bytes: &[u8]) -> Result<(), HoloCryptoError> {
+    if passphrase_bytes.len() > ARGON2_MAX_PASSPHRASE_LEN {
+        return Err(HoloCryptoError::Custom(
+            "Passphrase exceeds maximum allowable number of bytes".to_string(),
+        ))
+    } else {
+        return Ok(())
+    }
+}
+
+fn check_pubkey(hc_pub_key: &str) -> Result<(), HoloCryptoError> {
+    // TODO: Actually decode this properly
+    let hc_pub_key_bytes = hc_pub_key.as_bytes();
+    if hc_pub_key_bytes.len() != 32 {
+        return Err(HoloCryptoError::Custom(format!(
+            "Expected hc_pub_key to be 32 bytes long, found {}",
+            hc_pub_key_bytes.len()
+        )))
+    } else {
+        Ok(())
+    }
+}
+
+fn check_salt(salt_bytes: &[u8]) -> Result<(), HoloCryptoError> {
+    match salt_bytes.len() {
+        0..=ARGON2_MIN_SALT_LEN if salt_bytes.len() < ARGON2_MIN_SALT_LEN => Err(
+            HoloCryptoError::Custom("Salt must be at least 8 bytes".to_string()),
+        ),
+        ARGON2_MIN_SALT_LEN..=ARGON2_MAX_SALT_LEN => Ok(()),
+        _ => Err(HoloCryptoError::Custom(
+            "Salt exceeds maximum allowable number of bytes".to_string(),
+        )),
+    }
+}
 
 #[wasm_bindgen]
 pub struct AdminKeyPair(Keypair);
@@ -19,15 +68,28 @@ pub struct AdminKeyPair(Keypair);
 impl AdminKeyPair {
     #[wasm_bindgen(constructor)]
     pub fn new(email: &str, pass: &str, hc_pub_key: &str) -> Result<AdminKeyPair, JsValue> {
-        let hc_pub_key = PublicKey::from_bytes(hc_pub_key.as_bytes()).unwrap();
-        Ok(Self(admin_keypair_from(hc_pub_key, email, pass).unwrap()))
+        let hc_pub_key_bytes = hc_pub_key.as_bytes();
+        let passphrase_bytes = pass.as_bytes();
+
+        check_pubkey(hc_pub_key)?;
+        check_passphrase(passphrase_bytes)?;
+
+        let hc_pub_key = PublicKey::from_bytes(hc_pub_key_bytes)
+            .map_err(|e| HoloCryptoError::Custom(format!("{}", e)))?;
+        Ok(Self(
+            admin_keypair_from(hc_pub_key, email, pass)
+                .map_err(|e| HoloCryptoError::Custom(format!("{}", e)))?,
+        ))
     }
 
     #[wasm_bindgen]
     /// Returns a base64 encoded signature of the payload
     pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
         let sig_bytes = self.0.sign(payload.as_bytes()).to_bytes();
-        Ok(base64::encode_config(&sig_bytes[..], base64::STANDARD_NO_PAD))
+        Ok(base64::encode_config(
+            &sig_bytes[..],
+            base64::STANDARD_NO_PAD,
+        ))
     }
 }
 
@@ -45,18 +107,30 @@ impl WebUserKeyPair {
         salt: &str,
         happ_hash: &str,
     ) -> Result<WebUserKeyPair, JsValue> {
-        Ok(Self(keypair_from(
-            pass.as_bytes(),
-            salt.as_bytes(),
-            happ_hash.as_bytes(),
-            WEBUSER_ARGON_ADDITIONAL_DATA,
-        ).unwrap()))
+        let salt_bytes = salt.as_bytes();
+        let passphrase_bytes = pass.as_bytes();
+
+        check_passphrase(passphrase_bytes)?;
+        check_salt(salt_bytes)?;
+
+        Ok(Self(
+            keypair_from(
+                passphrase_bytes,
+                salt_bytes,
+                happ_hash.as_bytes(),
+                WEBUSER_ARGON_ADDITIONAL_DATA,
+            )
+            .map_err(|e| HoloCryptoError::Custom(format!("{}", e)))?,
+        ))
     }
 
     #[wasm_bindgen]
     /// Returns a base64 encoded signature of the payload
     pub fn sign(&self, payload: &str) -> Result<String, JsValue> {
         let sig_bytes = self.0.sign(payload.as_bytes()).to_bytes();
-        Ok(base64::encode_config(&sig_bytes[..], base64::STANDARD_NO_PAD))
+        Ok(base64::encode_config(
+            &sig_bytes[..],
+            base64::STANDARD_NO_PAD,
+        ))
     }
 }
--- src/holo-crypto-wasm/tests/web.rs
+++ src/holo-crypto-wasm/tests/web.rs
@@ -10,32 +10,41 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn admin_can_derive_key_and_sign() {
-    let akp = AdminKeyPair::new("some@email.test", "p4ssw0rd", "HCA01adefasdtbeodbgahfawnf")
-        .expect("Could not generate keypair");
+    let akp = AdminKeyPair::new(
+        "some@email.test",
+        "p4ssw0rd",
+        "HCA01adefasdtbeodbgahfawnfsdegds",
+    )
+    .expect("Could not generate keypair");
 
     assert_eq!(
         akp.sign("message").expect("Could not sign payload").len(),
-        88, // 64 bytes base64 encoded = 88 bits
+        86, // 64 bytes base64 encoded = 86 bits
     )
 }
 
 #[wasm_bindgen_test]
-fn admin_will_error_if_salt_too_short() {
+fn admin_will_error_if_hc_pub_key_incorrect_length() {
     assert!(AdminKeyPair::new("some@email.test", "pwd", "HCA01..").is_err())
 }
 
 #[wasm_bindgen_test]
 fn webuser_can_derive_key_and_sign() {
-    let akp = WebUserKeyPair::new("some@email.test", "p4ssw0rd", "saltysalt", "QmDnaHash...")
-        .expect("Could not generate keypair");
+    let akp = WebUserKeyPair::new(
+        "some@email.test",
+        "p4ssw0rd",
+        "saltsaltsalt",
+        "QmDnaHash...",
+    )
+    .expect("Could not generate keypair");
 
     assert_eq!(
         akp.sign("message").expect("Could not sign payload").len(),
-        88, // 64 bytes base64 encoded = 88 bits
+        86, // 64 bytes base64 encoded = 86 bits
     )
 }
 
 #[wasm_bindgen_test]
 fn webuser_will_error_if_salt_too_short() {
-    assert!(WebUserKeyPair::new("some@email.test", "pwd", "HCA01..", "QmDnaHash...").is_err())
+    assert!(WebUserKeyPair::new("some@email.test", "pwd", "salt", "QmDnaHash...").is_err())
 }
