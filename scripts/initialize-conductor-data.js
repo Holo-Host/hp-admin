34255f70385af370567210f65726862681e0d561
Robbie Carlton <robbie.carlton@gmail.com>
2019-10-31 12:52:37 -0600

    notes on seed data

--- scripts/initialize-conductor-data.js
+++ scripts/initialize-conductor-data.js
@@ -2,9 +2,18 @@ const createZomeCall = require('./create-zome-call')
 const { getAgent } = require('../src/utils/conductorConfig')
 
 async function populateData () {
+  const agent1 = getAgent()
+  const agent2 = getAgent(1)
+
   const zomeCall = await createZomeCall('holofuel', 'transactions', 'whoami')
   const result = await zomeCall()
   console.log('result of zome call', result)
+
+  // seed data:
+  // a request from agent2 to agent1
+  // an offer from agent2 to agent1
+  // a request from agent1 to agent2 which agent 2 has paid
+  // an offer from agent1 to agent2 which agent 2 has accepted
 }
 
 populateData()
