3edc461be99e427c87faf0242adf25cc3f4f7d0f
Lisa <ldjetton21@gmail.com>
2019-11-03 01:35:09 -0500

    removed ncp - underlying performance issues with chmod

--- src/holofuel/__integration_tests__/CreateRequest.integration.test.js
+++ src/holofuel/__integration_tests__/CreateRequest.integration.test.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { fireEvent, wait } from '@testing-library/react'
 import { renderAndWait } from 'utils/test-utils'
+import { mockNavigateTo } from 'react-router-dom'
 import { HoloFuelApp } from 'root'
 import { getAgent } from 'utils/conductorConfig'
-import runConductor from 'util/runConductorWithFixtures'
+import runConductor from 'utils/runConductorWithFixtures'
 
 jest.mock('react-media-hook')
 jest.mock('react-identicon-variety-pack')
+jest.unmock('react-router-dom')
 
 const agentId = getAgent().id
 
 describe('CreateRequest', () => {
   it('user can create a request and then view it in the transaction history', runConductor(async () => {
+    console.log('6')
+
     const amount = 123
 
     const { getByTestId, getByText, getByLabelText } = await renderAndWait(<HoloFuelApp />)
@@ -26,6 +30,7 @@ describe('CreateRequest', () => {
     fireEvent.change(getByLabelText('Amount'), { target: { value: amount } })
     fireEvent.click(getByText('Send'))
 
+    expect(mockNavigateTo).toHaveBeenCalledWith('/history')
     await wait(() => getByLabelText('History'))
   }), 20000)
 })
--- src/utils/runConductorWithFixtures.js
+++ src/utils/runConductorWithFixtures.js
@@ -1,30 +1,57 @@
 // import { exec } from 'child_process'
+const fs = require('fs')
+// const fsPromises = fs.promises
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
-const ncp = util.promisify(require('ncp').ncp)
+// const ncp = util.promisify(require('ncp').ncp)
+// const ncp = require('ncp').ncp
 const rimraf = require('rimraf')
 
 export default function runConductorWithFixtures (testFn) {
   return async function () {
     console.log('1')
     await exec('npm run hc:stop')
-      .catch(e => console.log('hc:stop error: NO HOLOCHAIN PROCESS EXISTS')) // console.log('hc:stop error: ', e)
+      .catch(e => console.log('hc:stop error: ', e))
 
     console.log('2')
-    rimraf.sync(process.env.REACT_APP_DEFAULT_STORAGE)
+    fs.access(process.env.REACT_APP_STORAGE_SNAPSHOT, fs.constants.F_OK, async (e) => {
+      if (e) console.log('Defaulting to residual Default Storage dir. \n Error locating Storage Snapshot dir : ', e)
+      else {
+        rimraf(process.env.REACT_APP_DEFAULT_STORAGE, e => {
+          if (e) {
+            console.error(e)
+            throw new Error('Error deleting residual Default Storage dir: ')
+          } else console.log('Deleted residual Default Storage dir.')
+        })
 
-    console.log('3')
-    await ncp(process.env.REACT_APP_STORAGE_SNAPSHOT, process.env.REACT_APP_DEFAULT_STORAGE)
+        console.log('3')
 
-    console.log('4')
+        const { stderr } = await exec(`cp -r ${process.env.REACT_APP_STORAGE_SNAPSHOT} ${process.env.REACT_APP_DEFAULT_STORAGE}`)
+        if (stderr) {
+          console.error(e)
+          throw new Error('Error coping Snapshot Storage dir into Default Storage dir: ')
+        } else {
+          console.log('Copied Snapshot Storage into Default Storage!')
+          console.log('4')
+        }
+
+        // await ncp(process.env.REACT_APP_STORAGE_SNAPSHOT, process.env.REACT_APP_DEFAULT_STORAGE)
+        //   ncp(process.env.REACT_APP_STORAGE_SNAPSHOT, process.env.REACT_APP_DEFAULT_STORAGE, { clobber: true }, e => {
+        //   if (e) {
+        //     console.error(e)
+        //     throw new Error('Error coping Snapshot Storage dir into Default Storage dir: ')
+        //   }
+        //   console.log('Copied Snapshot Storage into Default Storage!')
+        //   console.log('4')
+        // })
+      }
+    })
+
     const hcStart = async () => {
-      const { stderr, stdout } = await exec('npm run hc:start &')
-      console.log('hc:start stdout:', stdout)
+      const { stderr } = await exec('npm run hc:start &')
       if (stderr) throw new Error('hc:start stderr:', stderr)
     }
     hcStart()
-
-    console.log('4.5')
 
     const waitConductor = async () => {
       const { stderr } = await exec('npm run test:wait-for-conductor')
@@ -38,7 +65,5 @@ export default function runConductorWithFixtures (testFn) {
         return testFn()
       })
       .then(() => { return console.log('Scenario Test Complete') })
-
-    // TODO: test took 123 long to start conductor ...
   }
 }
