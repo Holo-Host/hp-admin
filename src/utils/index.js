201f95372f0c73e6c6f03a0f89625793c08c17f4
Lisa <ldjetton21@gmail.com>
2019-11-09 17:24:26 -0600

    updated email utils fn

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 import moment from 'moment'
 
-// export const EMAIL_REGEXP_CHECK = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// eslint-disable-next-line no-useless-escape
+export const FORMAT_EMAIL_REGEXP = (str) => str.replace(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
 export function bgImageStyle (url) {
   if (!url) return {}
