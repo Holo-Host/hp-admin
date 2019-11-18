c352b5115bff1c8a1525de9700e38582190969ba
Robbie Carlton <robbie.carlton@gmail.com>
2019-11-18 16:13:03 -0700

    remove mock message

--- src/contexts/useFlashMessageContext.js
+++ src/contexts/useFlashMessageContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react'
 export const FlashMessageContext = createContext()
 
 export function FlashMessageProvider ({ children }) {
-  const [message, setMessage] = useState('Permanent')
+  const [message, setMessage] = useState('')
   const [time, setTime] = useState()
   const newMessage = (message, time) => {
     setMessage(message)
