e00f15b7cd6769d7fa067ef2fb568307bdc85957
zo-el <joelulahanna@gmail.com>
2019-11-18 09:51:19 -0600

    updated to use global var

--- scripts/wait-for-conductor.js
+++ scripts/wait-for-conductor.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 const wait = require('waait')
-const { getAgent } = require('../src/utils/integration-testing/conductorConfig')
 
 const printWaitingFeedback = async (interval = 10000) => {
   console.log(`Pausing for ${Math.ceil(interval / 1000)} seconds`)
@@ -34,8 +33,7 @@ async function waitForConductor (interval = 10000) {
       console.log('Checking if conductor is up')
       // We need to check if the instance is live
       // By calling an admin call does not mean that the instances are active
-      const agent1 = getAgent(agent1Index)
-      const result = await axios.post(`http://localhost:3300/${agent1}::<happ_id>-holofuel/transactions/whoami`, {})
+      const result = await axios.post(`http://localhost:3300/${process.env.REACT_APP_AGENT_ID}::<happ_id>-holofuel/transactions/whoami`, {})
       if (result.status !== 200 && result.data.error) throw new Error(result.data.error.message)
       else if (result.status === 200) isUp = true
       else return result.status
