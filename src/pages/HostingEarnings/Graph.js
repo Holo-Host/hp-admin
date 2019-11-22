c679b87c84f8a6e4a493da49f9538fc8e2bf9ca6
Robbie Carlton <robbie.carlton@gmail.com>
2019-11-22 12:39:09 -0700

    style chart

--- src/pages/HostingEarnings/Graph.js
+++ src/pages/HostingEarnings/Graph.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { VictoryChart, VictoryBar, Bar } from 'victory'
-import { DAYS } from './HostingEarnings'
 import { flow, groupBy, values, map } from 'lodash/fp'
 import moment from 'moment'
+import { caribbeanGreen, silverChalice } from 'utils/colors'
+import { DAYS } from './HostingEarnings'
+import './Graph.module.css'
 
 const getBucket = days => transaction => {
   if (days === DAYS.one) {
@@ -26,16 +28,44 @@ function generateData (transactions, days) {
 export default function Graph ({ transactions, days }) {
   const data = generateData(transactions, days)
 
-  return <div>
-    <VictoryChart height={400} width={400}
-      domainPadding={{ x: 50, y: [0, 20] }}
+  const chartTheme = {
+    axis: {
+      style: {
+        axis: {
+          fill: 'none',
+          stroke: 'none'
+        },
+        tickLabels: {
+          fill: silverChalice,
+          padding: 10
+        },
+        grid: {
+          fill: 'none',
+          stroke: 'none',
+          pointerEvents: 'painted'
+        }
+      }
+    }
+  }
+
+  const barStyle = {
+    data: {
+      fill: caribbeanGreen
+    }
+  }
+
+  return <div styleName='graph'>
+    <VictoryChart height={400}
+      domainPadding={{ x: 25, y: [0, 20] }}
       scale={{ x: 'time' }}
+      theme={chartTheme}
     >
       <VictoryBar
         dataComponent={
           <Bar />
         }
         data={data}
+        style={barStyle}
       />
     </VictoryChart>
   </div>
