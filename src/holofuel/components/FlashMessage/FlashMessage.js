c325c4d842ba73258432d9914cf3d12a15b33c33
Lisa <ldjetton21@gmail.com>
2019-12-07 15:04:31 -0600

    update request page to include restricton for transaction with bogus agent

--- src/holofuel/components/FlashMessage/FlashMessage.js
+++ src/holofuel/components/FlashMessage/FlashMessage.js
@@ -19,8 +19,6 @@ export default function FlashMessage () {
 
   if (isEmpty(message)) return null
 
-  console.log(' INSIDE FLASH MESSAGE : ', message)
-
   return <div styleName='flash-message'>
     <div styleName='message'>{message}</div>
     <button styleName='close-button' onClick={clearMessage}>x</button>
