563a850ab9a6320802c0a6ca6b48668ce5e516aa
Lisa <ldjetton21@gmail.com>
2019-12-20 13:49:30 -0600

    adds useCallback in home; updates port for conductor two ws driver

--- src/holofuel/pages/Home/Home.js
+++ src/holofuel/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react' // useState,
+import React, { useEffect, useCallback } from 'react' // useState,
 import { useQuery } from '@apollo/react-hooks'
 import { useHistory, Link } from 'react-router-dom'
 import { isEmpty, get, uniqBy } from 'lodash/fp'
@@ -52,21 +52,14 @@ export default function Home () {
   const goToOfferRequest = () => history.push(OFFER_REQUEST_PATH)
 
   const { newMessage } = useFlashMessageContext()
-  // const [errorMessage, setErrorMessage] = useState(null)
 
-  // if (errorMessage) {
-  //   newMessage(errorMessage)
-  //   setErrorMessage('')
-  // }
-
-  const filterActionableTransactionsByStatus = status => holofuelActionableTransactions.filter(actionableTx => actionableTx.status === status)
-  // if (!isEmpty(filterActionableTransactionsByStatus('declined')) && errorMessage === null) setErrorMessage(declinedTransactionNotice)
+  const filterActionableTransactionsByStatus = useCallback(status => holofuelActionableTransactions.filter(actionableTx => actionableTx.status === status), [holofuelActionableTransactions])
 
   useEffect(() => {
     if (!isEmpty(filterActionableTransactionsByStatus('declined'))) {
       newMessage(declinedTransactionNotice)
     }
-  }, [filterActionableTransactionsByStatus])
+  }, [filterActionableTransactionsByStatus, newMessage])
 
   return <PrimaryLayout headerProps={{ title: 'Home' }}>
     <div styleName='avatar'>
