876e0badb0a973e091d4d31a1a8d05076082c0fc
zo-el <joelulahanna@gmail.com>
2019-12-23 00:17:05 -0600

    update

--- src/holofuel/components/layout/PrimaryLayout/PrimaryLayout.js
+++ src/holofuel/components/layout/PrimaryLayout/PrimaryLayout.js
@@ -22,6 +22,7 @@ export function PrimaryLayout ({
   const { loading: holofuelUserLoading, data: { holofuelUser = {} } = {} } = useQuery(HolofuelUserQuery)
   const { data: { holofuelLedger: { balance: holofuelBalance } = { balance: 0 } } = {} } = useQuery(HolofuelLedgerQuery, { fetchPolicy: 'network-only' })
 
+
   const inboxCount = actionableTransactions.filter(actionableTx => (actionableTx.status !== STATUS.canceled) && (actionableTx.type !== TYPE.request)).length
 
   const isWide = useContext(ScreenWidthContext)
