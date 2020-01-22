789c225a8ceaaea84eadf7520ca528bc3617afbc
Lisa <ldjetton21@gmail.com>
2020-01-21 19:09:40 -0600

    updated agents to v 0.0.24-4

--- scripts/create-zome-call.js
+++ scripts/create-zome-call.js
@@ -1,6 +1,7 @@
 const initAndGetHolochainClient = require('./holochain-client.js')
 
 function createZomeCall (instanceId, zomeName, functionName, agentIndex = 0) {
+  console.log('agentIndex : ', agentIndex)
   return async function (args = {}) {
     const hc = await initAndGetHolochainClient(agentIndex)
     return hc.callZome(instanceId, zomeName, functionName)(args)
