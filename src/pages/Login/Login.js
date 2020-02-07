4825162af61dcc98293c898892a20ad461e23d02
Lisa <ldjetton21@gmail.com>
2020-02-06 20:41:12 -0600

    cleaned

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useMutation } from '@apollo/react-hooks'
 import useForm from 'react-hook-form'
 import { get } from 'lodash/fp'
