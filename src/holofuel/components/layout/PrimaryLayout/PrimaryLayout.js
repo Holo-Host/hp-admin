bbd2ba4e2b650880709426ef779ec8f71dd042d9
jetttech <lisa.jetton@holo.host>
2020-06-25 13:04:23 -0500

    ensure acceptRequestedOffers only accepts pending payments

--- src/holofuel/components/layout/PrimaryLayout/PrimaryLayout.js
+++ src/holofuel/components/layout/PrimaryLayout/PrimaryLayout.js
@@ -5,6 +5,7 @@ import cx from 'classnames'
 import { useHistory } from 'react-router-dom'
 import HolofuelActionableTransactionsQuery from 'graphql/HolofuelActionableTransactionsQuery.gql'
 import HolofuelCompletedTransactionsQuery from 'graphql/HolofuelCompletedTransactionsQuery.gql'
+import HolofuelNonPendingTransactionsQuery from 'graphql/HolofuelNonPendingTransactionsQuery.gql'
 import HolofuelLedgerQuery from 'graphql/HolofuelLedgerQuery.gql'
 import HolofuelUserQuery from 'graphql/HolofuelUserQuery.gql'
 import ScreenWidthContext from 'holofuel/contexts/screenWidth'
@@ -31,6 +32,7 @@ function PrimaryLayout ({
   const { loading: ledgerLoading, data: { holofuelLedger: { balance: holofuelBalance } = {} } = {}, stopPolling: stopPollingLedger, startPolling: startPollingLedger } = useQuery(HolofuelLedgerQuery, { fetchPolicy: 'cache-and-network', pollInterval: 5000 })
   const { data: { holofuelActionableTransactions: actionableTransactions = [] } = {}, stopPolling: stopPollingActionableTransactions, startPolling: startPollingActionableTransactions } = useQuery(HolofuelActionableTransactionsQuery, { fetchPolicy: 'cache-and-network' })
   const { stopPolling: stopPollingCompletedTransactions, startPolling: startPollingCompletedTransactions } = useQuery(HolofuelCompletedTransactionsQuery, { fetchPolicy: 'cache-and-network' })
+  const { stopPolling: stopPollingNonPendingTransactions, startPolling: startPollingNonPendingTransactions } = useQuery(HolofuelNonPendingTransactionsQuery, { fetchPolicy: 'cache-and-network' })
   const { refetch: refetchUser } = useQuery(HolofuelUserQuery, { fetchPolicy: 'cache-and-network' })
   const { currentUser, currentUserLoading } = useCurrentUserContext()
   const { isConnected, setIsConnected } = useConnectionContext()
@@ -55,6 +57,7 @@ function PrimaryLayout ({
         defaultPath = HOME_PATH
         stopPollingActionableTransactions()
         stopPollingCompletedTransactions()
+        stopPollingNonPendingTransactions()
         setShouldRefetchUser(true)
       } else {
         connectionErrorMessage = 'Your Holoport is currently unreachable.'
@@ -69,6 +72,7 @@ function PrimaryLayout ({
       newMessage('', 0)
       startPollingActionableTransactions(5000)
       startPollingCompletedTransactions(5000)
+      startPollingNonPendingTransactions(5000)
       if (shouldRefetchUser) {
         refetchHolofuelUser()
       }
@@ -81,6 +85,8 @@ function PrimaryLayout ({
     startPollingActionableTransactions,
     stopPollingCompletedTransactions,
     startPollingCompletedTransactions,
+    stopPollingNonPendingTransactions,
+    startPollingNonPendingTransactions,
     stopPollingLedger,
     startPollingLedger,
     shouldRefetchUser,
