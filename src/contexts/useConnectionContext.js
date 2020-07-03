1391a4ec53ac246b2c2b24073ee41383da2a7e00
Robbie Carlton <robbie.carlton@gmail.com>
2020-07-03 14:11:37 -0600

    cleanup

--- src/contexts/useConnectionContext.js
+++ src/contexts/useConnectionContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react'
 export const ConnectionContext = createContext()
 
 export function ConnectionProvider ({ children }) {
-  const [isConnected, setIsConnected] = useState(true)
+  const [isConnected, setIsConnected] = useState(false)
 
   return <ConnectionContext.Provider value={{ isConnected, setIsConnected }}>
     {children}
