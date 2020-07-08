8b2eabda990af3f6140248629132b39c4287b6f2
jetttech <lisa.jetton@holo.host>
2020-07-08 06:57:18 -0500

    update flash message with viewport width

--- src/holofuel/components/FlashMessage/FlashMessage.js
+++ src/holofuel/components/FlashMessage/FlashMessage.js
@@ -1,9 +1,12 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useContext, useCallback, useEffect } from 'react'
 import { isEmpty } from 'lodash/fp'
+import cx from 'classnames'
+import ScreenWidthContext from 'holofuel/contexts/screenWidth'
 import useFlashMessageContext from 'holofuel/contexts/useFlashMessageContext'
 import './FlashMessage.module.css'
 
 export default function FlashMessage () {
+  const isWide = useContext(ScreenWidthContext)
   const { message, time, newMessage } = useFlashMessageContext()
   const clearMessage = useCallback(
     () => newMessage('', 0),
@@ -19,7 +22,7 @@ export default function FlashMessage () {
 
   if (isEmpty(message)) return null
 
-  return <div styleName='flash-message'>
+  return <div styleName={cx('flash-message', { 'desktop': isWide })}>
     <div styleName='flash-body'>{message}</div>
     <button styleName='close-button' onClick={clearMessage}>x</button>
   </div>
