653fc44d811d6deadb6fbc815344db890e39a3c4
jetttech <lisa.jetton@holo.host>
2020-07-12 22:33:37 -0500

    correct icon navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom'
 import './Header.module.css'
 import BackIcon from 'components/icons/BackIcon'
 import GearIcon from 'components/icons/GearIcon'
-import { HP_ADMIN_SETTINGS } from 'utils/urls'
+import { ROOT, HP_ADMIN_DASHBOARD, HP_ADMIN_SETTINGS } from 'utils/urls'
 
-export default function Header ({ title }) {
+export default function Header ({ title }) {  
   return <div styleName='header'>
-    <Link to='/admin/dashboard' styleName='avatar-link' data-testid='avatar-link'>
-      {window.location.pathname === HP_ADMIN_SETTINGS && <BackIcon styleName='back-icon' />}
+    {window.location.pathname === HP_ADMIN_SETTINGS
+      ? <Link to='/admin/dashboard' styleName='avatar-link' data-testid='avatar-link'>
+      <BackIcon styleName='back-icon' />
     </Link>
+    : <div />}
     <h1 styleName='title'>{title}</h1>
     <div styleName='left-nav'>
-      <Link to='/admin/settings' styleName='settings-link'>
+      {(window.location.pathname === HP_ADMIN_DASHBOARD || window.location.pathname === ROOT) && <Link to='/admin/settings' styleName='settings-link'>
         <GearIcon styleName='gear-icon' />
-      </Link>
+      </Link>}
     </div>
   </div>
 }
