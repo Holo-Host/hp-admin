143ad65939d5b69ac228fa787f3816c442fc474d
jetttech <lisa.jetton@holo.host>
2020-07-22 10:06:17 -0500

    update polling_interval_cli

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Default polling interval to 30000ms
-POLLING_INTERVAL=${1:-'30000'}
+CLI_POLLING_INTERVAL=null
 
-# Check to verify that POLLING_INTERVAL is comprised of numbers
-case $POLLING_INTERVAL in
+if [ $1 ]; then
+  # Check to verify that CLI_POLLING_INTERVAL is comprised of numbers
+  case $1 in
     ''|*[!0-9]*) echo "Command Usage Error: Argument 1 must be a number" >&2; exit 1 ;;
     *)
-esac
+  esac
+  # Set polling var if passed in
+  CLI_POLLING_INTERVAL=$1
+fi
 
 rm -rf ./node_modules/.cache/babel-loader
-
-REACT_APP_VERSION=$npm_package_version REACT_APP_POLLING_INTERVAL=$POLLING_INTERVAL node scripts/build.js
+REACT_APP_VERSION=$npm_package_version REACT_APP_CLI_POLLING_INTERVAL=$CLI_POLLING_INTERVAL node scripts/build.js
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import { flow, groupBy, keys, sortBy, reverse } from 'lodash/fp'
 import moment from 'moment'
 
 // Default polling interval to 30000ms
-export const POLLING_INTERVAL = parseInt(process.env.REACT_APP_POLLING_INTERVAL) || 30000
+export const POLLING_INTERVAL = parseInt(process.env.REACT_APP_CLI_POLLING_INTERVAL) || 30000
 export const POLLING_INTERVAL_GENERAL = Math.max(Math.floor(POLLING_INTERVAL), 5000)
 export const POLLING_INTERVAL_SETTINGS = Math.max(Math.floor(POLLING_INTERVAL / 3), 5000)
 
