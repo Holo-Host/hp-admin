4ca81deda21782fd3a37bfdf19fc38880b99f76d
jetttech <lisa.jetton@holo.host>
2020-07-31 03:32:11 -0500

    added integer threshold of 1 trillion

--- src/holofuel/pages/CreateOfferRequest/AmountInput/AmountInput.js
+++ src/holofuel/pages/CreateOfferRequest/AmountInput/AmountInput.js
@@ -8,43 +8,55 @@ import './AmountInput.module.css'
 // NB: The sum of the intgr and fraction cannot exceed 19 and be within a valid holofuel range
 const INTEGER_PLACEVALUE_LIMIT = 13
 const FRACTION_PLACEVALUE_LIMIT = 6
+const integerThresholdError = 'The max whole number amount per a single transaction is 1 trillion TF'
 
 const useParseHolofuelAmount = () => {
   return stringAmount => {
     const hasDot = /\./.test(stringAmount)
     const [integer, fraction] = stringAmount.split('.')
+    const parsedInteger = Number(integer).toString()
     let hasAmountError = ''
 
+    const exceedsOneTrillion = amount => {
+      return Number(amount) > 1000000000000
+    }
+    
+    if (parsedInteger && exceedsOneTrillion(parsedInteger)) {
+      // throw error if integer is higher than 1 trillion
+      hasAmountError = integerThresholdError
+    }
+    
     let verifiedInteger, verifiedFraction
-    const parsedInteger = Number(integer).toString()
     verifiedInteger = !isNaN(parsedInteger) ? parsedInteger : ''
     verifiedFraction = fraction
-
+    
     if (parsedInteger && parsedInteger.length > INTEGER_PLACEVALUE_LIMIT) {
-      // throw error if integer exceeds trillions (13 digits)
-      hasAmountError = 'The max transaction amount per a single transaction is 1 trillion TF'
+      // throw error if integer exceeds 13 digits (trillions)
+      hasAmountError = `Your transaction cannot exceed ${INTEGER_PLACEVALUE_LIMIT} whole numbers`
       verifiedInteger = parsedInteger.substring(0, INTEGER_PLACEVALUE_LIMIT)
     }
-
+    
     if (fraction && fraction.length > FRACTION_PLACEVALUE_LIMIT) {
       // throw error if decimal exceeds 6 place values of percision (one-millionths)
       hasAmountError = `Your transaction amount cannot exceed ${FRACTION_PLACEVALUE_LIMIT} decimals`
       verifiedFraction = fraction.substring(0, FRACTION_PLACEVALUE_LIMIT)
     }
-
+    
     const amount = (hasDot && verifiedFraction)
-      ? verifiedInteger + '.' + verifiedFraction
-      : hasDot
-        ? verifiedInteger + '.'
-        : verifiedInteger || verifiedFraction
-
+    ? verifiedInteger + '.' + verifiedFraction
+    : hasDot
+    ? verifiedInteger + '.'
+    : verifiedInteger || verifiedFraction
+    
     const presentedAmount = (hasDot && verifiedFraction)
-      ? Number(verifiedInteger).toLocaleString() + '.' + verifiedFraction
-      : hasDot
-        ? Number(verifiedInteger).toLocaleString() + '.'
-        : Number(verifiedInteger).toLocaleString() || verifiedFraction
-
-    return { amount, presentedAmount, hasAmountError }
+    ? Number(verifiedInteger).toLocaleString() + '.' + verifiedFraction
+    : hasDot
+    ? Number(verifiedInteger).toLocaleString() + '.'
+    : Number(verifiedInteger).toLocaleString() || verifiedFraction
+    
+    const exceedsCeiling = exceedsOneTrillion(verifiedInteger)
+ 
+    return { amount, presentedAmount, hasAmountError, exceedsCeiling }
   }
 }
 
@@ -52,17 +64,31 @@ export default function AmountInput ({ amount, setAmount, chooseSend, chooseRequ
   const [presentedValue, setPresentedValue] = useState(String(amount))
   const [inputValue, setInputValueRaw] = useState(String(amount))
   const [amountError, setAmountError] = useState('')
+  const [isValidAmount, setIsValidAmount] = useState(amount > 0)
 
-  const isValidAmount = amount > 0
   const parseHolofuelAmount = useParseHolofuelAmount()
 
   const setInputValue = value => {
     const cleanValue = value.replace(/[^0-9.]/g, '') || '0' // strips non numerical characters
-    const { amount, presentedAmount, hasAmountError } = parseHolofuelAmount(cleanValue)
+    const { amount, presentedAmount, hasAmountError, exceedsCeiling } = parseHolofuelAmount(cleanValue)
     setPresentedValue(presentedAmount)
     setInputValueRaw(amount)
     setAmount(amount, presentedAmount)
     setAmountError(hasAmountError)
+
+    // refresh error message after 5s
+    if (hasAmountError && exceedsCeiling) {
+      setTimeout(() => { setAmountError(integerThresholdError) }, 5000)
+    } else if (hasAmountError) {
+      setTimeout(() => { setAmountError('') }, 5000)
+    }
+
+    // prevent proceeding past numpad if amount exceeds TF ceiling
+    if (exceedsCeiling) {
+      setIsValidAmount(false)
+    } else {
+      setIsValidAmount(amount > 0)
+    }
   }
 
   const addDigit = digit => () => {
