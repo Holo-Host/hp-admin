6c7e6fdcdc5102437b94c98299325a43cd19d663
jetttech <lisa.jetton@holo.host>
2020-07-31 02:15:34 -0500

    hp-admin fixes: alpha banner, settings page link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import GearIcon from 'components/icons/GearIcon'
 import ArrowRightIcon from 'components/icons/ArrowRightIcon'
+import { isPage } from 'utils/urls'
 import './Header.module.css'
 
 export default function Header ({ title, settings, showBackButton }) {
@@ -10,10 +11,14 @@ export default function Header ({ title, settings, showBackButton }) {
       <ArrowRightIcon styleName='arrow-icon' color={'#979797'} />
       <div styleName='back-text'>Back</div>
     </Link>}
+    {/* Q1: Why is the default for the agent's nicknae, 'HoloPort'? */}
+    {/* Q2: What happened to the option to rename your agent? */}
     {!showBackButton && <div styleName='nickname'>{settings.hostName || 'HoloPort'}</div>}
     <h1 styleName='title'>{title}</h1>
-    <Link to='/admin/settings' styleName='settings-link' data-testid='settings-link'>
-      <GearIcon />
-    </Link>
+    { !isPage('settings', window)
+      ? <Link to='/admin/settings' styleName='settings-link' data-testid='settings-link'>
+        <GearIcon />
+      </Link>
+      : < div styleName='settings-link'/>}
   </div>
 }
