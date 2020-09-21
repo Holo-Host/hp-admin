5a710c515a9792f26421fe3e7ee39ec4a1f9a75d
jetttech <lisa.jetton@holo.host>
2020-09-21 01:09:51 -0500

    clean up unit conftg - test

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -4,8 +4,5 @@ module.exports = {
   ...config,
   testMatch: [
     '<rootDir>/src/**/(*.unit).{spec,test}.{js,jsx,ts,tsx}'
-    // '<rootDir>/src/**/!(__integration_tests__)/**/*.{js,jsx,ts,tsx}' // ,
-    // '<rootDir>/src/**/!(*.hcIntegration).{spec,test}.{js,jsx,ts,tsx}',
-    // '<rootDir>/src/**/!(*.holoIntegration).{spec,test}.{js,jsx,ts,tsx}'
   ]
 }
